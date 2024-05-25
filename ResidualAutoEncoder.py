80ccf1a7fa8bdec881014f2da150123b5bc11aeb
Arjun Subramaniam <assubram@purdue.edu>
2024-05-25 01:03:00 -0400

    Working train harness

--- ResidualAutoEncoder.py
+++ ResidualAutoEncoder.py
@@ -67,11 +67,11 @@ class ResidualDecoderBlock(nn.Module):
         return out
     
 class ResidualEncoder(nn.Module):
-    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128):
+    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128, channels=3):
         super(ResidualEncoder, self).__init__()
 
         encoder_layers = []
-        in_channels = 3
+        in_channels = channels
         out_channels = 64
         for i in range(num_blocks):
             encoder_layers.append(ResidualEncoderBlock(in_channels, out_channels, num_layers=block_depth))
@@ -90,6 +90,8 @@ class ResidualEncoder(nn.Module):
             nn.ReLU(inplace=True)
         )
 
+    
+
     def forward(self, x):
         x = self.encoder(x)
         x = self.bottleneck_encoder(x)
@@ -97,7 +99,7 @@ class ResidualEncoder(nn.Module):
     
 
 class ResidualDecoder(nn.Module):
-    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128):
+    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128, channels=3):
         super(ResidualDecoder, self).__init__()
 
         in_channels = 64 * (2 ** (num_blocks - 1))
@@ -120,7 +122,7 @@ class ResidualDecoder(nn.Module):
             out_channels //= 2
         
         self.decoder = nn.Sequential(*decoder_layers)
-        self.final_conv = nn.Conv2d(in_channels, 3, kernel_size=1)
+        self.final_conv = nn.Conv2d(in_channels, channels, kernel_size=1)
 
     def forward(self, x):
         x = self.bottleneck_decoder(x)
@@ -129,16 +131,76 @@ class ResidualDecoder(nn.Module):
         return x
 
 class ResidualAutoencoder(nn.Module):
-    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128):
+    def __init__(self, num_blocks=3, block_depth=2, bottleneck_dim=128, input_size=128, channels=3, asym_block=1, asym_depth=1):
         super(ResidualAutoencoder, self).__init__()
         
-        self.ResidualEncoder = ResidualEncoder(num_blocks=num_blocks, block_depth=block_depth, bottleneck_dim=bottleneck_dim, input_size=input_size)
-        self.ResidualDecoder = ResidualDecoder(num_blocks=num_blocks, block_depth=block_depth, bottleneck_dim=bottleneck_dim, input_size=input_size)
+        self.ResidualEncoder = ResidualEncoder(num_blocks=num_blocks, block_depth=block_depth, bottleneck_dim=bottleneck_dim, input_size=input_size, channels=channels)
+        self.ResidualDecoder = ResidualDecoder(num_blocks=num_blocks * asym_block, block_depth=block_depth * asym_depth, bottleneck_dim=bottleneck_dim, input_size=input_size, channels=channels)
         
     def forward(self, x):
         x = self.ResidualEncoder(x)
         x = self.ResidualDecoder(x)
         return x
+    
+    def encode(self, x):
+        return self.ResidualEncoder(x)
+    
+    def decode(self, x):
+        return self.ResidualDecoder(x)
+    
+    def train_harness(self,model, train_loader, criterion, optimizer, device, epochs=10, l1_lambda= 0.0001, l1_lambda_bottleneck=0.01):
+        model.train()
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
+        for epoch in range(epochs):
+            train_loss = 0.0
+            for data in train_loader:
+                inputs, _ = data
+
+                inputs = inputs.to(device)
+                
+                # Forward pass
+                outputs = model(inputs)
+                loss = criterion(outputs, inputs)
+                loss = self.l1_reg(model, loss, l1_lambda=l1_lambda, l1_lambda_bottleneck=l1_lambda_bottleneck)
+                
+                # Backward pass and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+                train_loss += loss.item() * inputs.size(0)
+           
+            # Calculate average loss
+            train_loss /= len(train_loader.dataset)
+            scheduler.step(train_loss)
+            print(f"Epoch [{epoch + 1}/{epochs}], Loss: {train_loss:.4f}, learning rate: {scheduler.get_last_lr()[0]}")
+            
+            torch.cuda.empty_cache()
+
+    def evaluate_harness(self,model, test_loader, device):
+        model.eval()
+        with torch.no_grad():
+            data_iter = iter(test_loader)
+            inputs, _ = next(data_iter)
+            inputs = inputs.to(device)
+            outputs = model(inputs)
+            return inputs.cpu(), outputs.cpu()
+    
+    def l1_reg(self, model, loss, l1_lambda=0.001, l1_lambda_bottleneck=0.01):
+        L1_term = torch.tensor(0., requires_grad=True)
+        nweights = 0
+        for name, weights in model.named_parameters():
+            if 'bias' not in name:
+                weights_sum = torch.sum(torch.abs(weights))
+                L1_term = L1_term + weights_sum
+                nweights = nweights + weights.numel()
+
+        L1_term = L1_term / nweights
+        
+        # Regularize loss using L1 regularization
+        loss = loss - L1_term * l1_lambda if 'bottleneck' not in name else loss - L1_term * l1_lambda_bottleneck
+        
+        return loss
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -176,6 +238,7 @@ def train_autoencoder(config=None):
                 # Forward pass
                 outputs = model(inputs)
                 loss = criterion(outputs, inputs)
+                
                 
                 # Backward pass and optimization
                 optimizer.zero_grad()
@@ -230,43 +293,14 @@ sweep_config = {
 # Run sweep
 #wandb.agent(sweep_id, train_autoencoder)
 
-def train(model, train_loader, criterion, optimizer, device, epochs=10):
-    model.train()
-    for epoch in range(epochs):
-        train_loss = 0.0
-        for data in train_loader:
-            inputs, _ = data
-            inputs = inputs.to(device)
-            
-            # Forward pass
-            outputs = model(inputs)
-            loss = criterion(outputs, inputs)
-            
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            train_loss += loss.item() * inputs.size(0)
-        
-        # Calculate average loss
-        train_loss /= len(train_loader.dataset)
-        
-        print(f"Epoch [{epoch + 1}/{epochs}], Loss: {train_loss:.4f}")
 
-def evaluate(model, test_loader, device):
-    model.eval()
-    with torch.no_grad():
-        data_iter = iter(test_loader)
-        inputs, _ = next(data_iter)
-        inputs = inputs.to(device)
-        outputs = model(inputs)
-        return inputs.cpu(), outputs.cpu()
+
+
 
 def main():
     # Hyperparameters
-    batch_size = 32
-    learning_rate = 0.000001
+    batch_size = 64
+    learning_rate = 0.001
     epochs = 30
     
     # Device configuration
@@ -275,25 +309,25 @@ def main():
     # Data transformation and loading
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Normalize((0.5), ( 0.5)),
         transforms.Resize((128, 128))
     ])
-    train_dataset = datasets.Flowers102(root='./data', split='train', download=True, transform=transform)
-    test_dataset = datasets.Flowers102(root='./data', split='test', download=True, transform=transform)
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     
     # Model, loss function, and optimizer
-    model = ResidualAutoencoder(num_blocks=3, block_depth=2, bottleneck_dim=52).to(device)
+    model = ResidualAutoencoder(num_blocks=3, block_depth=2, bottleneck_dim=128, channels=3).to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
     # Train the model
-    train(model, train_loader, criterion, optimizer, device, epochs)
+    model.train_harness(model, train_loader, criterion, optimizer, device, epochs)
     
     # Evaluate the model
-    inputs, outputs = evaluate(model, test_loader, device)
+    inputs, outputs = model.evaluate_harness(model, test_loader, device)
     outputs = unnormalize(outputs)
     # Visualize the results
     num_images = 10
